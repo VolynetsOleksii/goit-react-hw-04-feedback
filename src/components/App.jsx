e3f9c7d586656f7680245f2a02281b9b77061faf
VolynetsOleksii <volinetsaleksey@gmail.com>
2023-05-19 14:36:22 +0300

    add app with hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,58 +1,58 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StatisticList } from './StatisticList/StatisticList';
 import { Section } from './Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Container } from './Container/Container.styled';
 import { GlobalStyle } from './GlobalStyle';
+import { useState } from 'react';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onSetFeedback = option => {
+    switch (option) {
+      case 'good':
+        return setGood(prevState => prevState + 1);
+      case 'neutral':
+        return setNeutral(prevState => prevState + 1);
+      case 'bad':
+        return setBad(prevState => prevState + 1);
+      default:
+        throw new Error(`Unsupported type of ${option}`);
+    }
   };
 
-  onSetFeedback = option => {
-    this.setState(prevState => {
-      return {
-        [option]: prevState[option] + 1,
-      };
-    });
-  };
-
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
     return good + neutral + bad;
   };
 
-  countPositivePercentage = () => {
-    const { good } = this.state;
-    return Math.round((good / this.countTotalFeedback()) * 100) || 0;
+  const countPositivePercentage = () => {
+    return Math.round((good / countTotalFeedback()) * 100) || 0;
   };
 
-  render() {
-    const keys = Object.keys(this.state);
-    const { good, neutral, bad } = this.state;
-
-    return (
-      <Container>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={keys}
-            onLeaveFeedback={this.onSetFeedback}
-          ></FeedbackOptions>
-        </Section>
-        <Section title="Statistics">
-          <StatisticList
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={this.countTotalFeedback()}
-            positivePercentage={this.countPositivePercentage()}
-          />
-        </Section>
-        <GlobalStyle />
-      </Container>
-    );
-  }
-}
+  const feedbackOption = { good, neutral, bad };
+  const keys = Object.keys(feedbackOption);
+  
+  return (
+    <Container>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={keys}
+          onLeaveFeedback={onSetFeedback}
+        ></FeedbackOptions>
+      </Section>
+      <Section title="Statistics">
+        <StatisticList
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositivePercentage()}
+        />
+      </Section>
+      <GlobalStyle />
+    </Container>
+  );
+};
